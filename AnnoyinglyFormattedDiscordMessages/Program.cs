7138881a291feb0d29cdede5c3ae80f1239daf77
Redstonia <redstonia1234@gmail.com>
2022-03-25 11:39:20 +1300

    Add project files.

---
+++ AnnoyinglyFormattedDiscordMessages/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace AnnoyinglyFormattedDiscordMessages
+{
+    class Program
+    {
+        public static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Type in your message below to be formatted");
+                string message = Console.ReadLine();
+                string formatted = "";
+
+                foreach (char c in message)
+                {
+                    string addition = c.ToString();
+                    if (Coin())
+                    {
+                        addition = "`" + addition + "`";
+                    }
+
+                    if (Coin())
+                    {
+                        addition = "_" + addition + "_";
+                    }
+
+                    if (Coin())
+                    {
+                        addition = "**" + addition + "**";
+                    }
+
+                    if (Coin())
+                    {
+                        addition = "__" + addition + "__";
+                    }
+
+                    formatted += addition + " ";
+                }
+
+                Console.WriteLine($"\nFormatted successfully!\n{formatted}\n");
+            }
+        }
+
+        public static bool Coin()
+        {
+            return rnd.Next(0, 2) == 0;
+        }
+    }
+}
